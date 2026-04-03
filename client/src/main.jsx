0442a04daae0d880e2f70ca9564623a11eeb29d3
Alex <alexcz@outlook.com>
2026-04-02 20:28:57 -0700

    livechat

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,60 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 import App from "./App.jsx";
 
+function initLiveChat() {
+	if (!import.meta.env.PROD || typeof window === "undefined") return;
+	if (window.LiveChatWidget || document.getElementById("livechat-tracking-script")) return;
+
+	window.__lc = window.__lc || {};
+	window.__lc.license = 19626871;
+	window.__lc.integration_name = "manual_onboarding";
+	window.__lc.product_name = "livechat";
+
+	(function (n, t, c) {
+		function i(args) {
+			return e._h ? e._h.apply(null, args) : e._q.push(args);
+		}
+
+		const e = {
+			_q: [],
+			_h: null,
+			_v: "2.0",
+			on: function () {
+				i(["on", c.call(arguments)]);
+			},
+			once: function () {
+				i(["once", c.call(arguments)]);
+			},
+			off: function () {
+				i(["off", c.call(arguments)]);
+			},
+			get: function () {
+				if (!e._h) throw new Error("[LiveChatWidget] You can't use getters before load.");
+				return i(["get", c.call(arguments)]);
+			},
+			call: function () {
+				i(["call", c.call(arguments)]);
+			},
+			init: function () {
+				const script = t.createElement("script");
+				script.id = "livechat-tracking-script";
+				script.async = true;
+				script.type = "text/javascript";
+				script.src = "https://cdn.livechatinc.com/tracking.js";
+				t.head.appendChild(script);
+			},
+		};
+
+		if (!n.__lc.asyncInit) {
+			e.init();
+		}
+
+		n.LiveChatWidget = n.LiveChatWidget || e;
+	})(window, document, [].slice);
+}
+
+initLiveChat();
+
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<App />
