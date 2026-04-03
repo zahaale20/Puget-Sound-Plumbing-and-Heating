7acb6630fa1bc78e5bf119077c4e536395a575da
Alex <alexcz@outlook.com>
2026-04-02 20:49:09 -0700

    blog post bg

--- client/src/pages/BlogPostPage.jsx
+++ client/src/pages/BlogPostPage.jsx
@@ -131,7 +131,7 @@ export default function BlogPostPage() {
 				</button>
 
 				{/* Blog Post Content */}
-				<article className="bg-white overflow-hidden">
+				<article className="overflow-hidden">
 					{/* Featured Image */}
 					{postImageUrl ? (
 						<img
